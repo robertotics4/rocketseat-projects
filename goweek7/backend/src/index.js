b5e3c4c81d5c22ca405c718259181f3c532d7b6b
Roberto Oliveira <robertooliveiragp@gmail.com>
2019-06-18 16:53:19 -0300

    Iniciando o backend

--- goweek7/backend/src/index.js
+++ goweek7/backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@mongodb-robs-womj6.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, }
+);
 
 app.listen(3333, () => {
     console.log('Servidor iniciado na porta 3333.');
